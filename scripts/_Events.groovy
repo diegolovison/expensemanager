c0fb72813eb3baf63c7a134e2e49c91bf0109cbd
Diego Lovison <diegolovison@gmail.com>
2014-05-24 22:26:16 -0300

    sencha touch war

---
+++ scripts/_Events.groovy
@@ -0,0 +1,18 @@
+eventCreateWarStart = { warName, stagingDir ->
+
+    compileSenchaApp()
+}
+
+void compileSenchaApp() {
+
+    def ant = new AntBuilder()
+
+    ant.exec(
+        dir: 'web-app',
+        executable:'sencha'
+    ) {
+        arg(value:'app')
+        arg(value:'build')
+        arg(value:'production')
+    }
+}
